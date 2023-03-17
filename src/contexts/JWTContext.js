37e456195ef0da09899b3c07db32c647c528aa1f
Khai Do <dooanhkhai@outlook.com>
2023-03-17 18:22:58 +0700

    commit

---
+++ src/contexts/JWTContext.js
@@ -0,0 +1,190 @@
+import { createContext, useEffect, useReducer } from 'react';
+// utils
+import { isValidToken, setSession } from '../utils/jwt';
+// @types
+import { ActionMap, AuthState, AuthUser } from '../@types/authentication';
+import FirebaseService from 'api/firebase';
+import { REACT_APP_API_URL } from 'config';
+import axios from 'axios';
+
+// ----------------------------------------------------------------------
+
+const Types = {
+  Initial: 'INITIALIZE',
+  Login: 'LOGIN',
+  Logout: 'LOGOUT',
+  Register: 'REGISTER'
+};
+
+const initialState = {
+  isAuthenticated: false,
+  isInitialized: false,
+  user: null
+};
+
+const JWTReducer = (state, action) => {
+  switch (action.type) {
+    case 'INITIALIZE':
+      return {
+        isAuthenticated: action.payload.isAuthenticated,
+        isInitialized: true,
+        user: action.payload.user
+      };
+    case 'LOGIN':
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user
+      };
+    case 'LOGOUT':
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null
+      };
+
+    default:
+      return state;
+  }
+};
+
+const AuthContext = createContext(null);
+
+function AuthProvider({ children }) {
+  const [state, dispatch] = useReducer(JWTReducer, initialState);
+
+  useEffect(() => {
+    const initialize = async () => {
+      try {
+        const accessToken = window.localStorage.getItem('accessToken');
+        const userId = window.localStorage.getItem('userId');
+        if (accessToken && userId && isValidToken(accessToken)) {
+          const response = await axios.get(REACT_APP_API_URL + `users/${userId}`, {
+            headers: { Authorization: `Bearer ${accessToken}` }
+          });
+          const {
+            id,
+            email,
+            image,
+            phoneNum,
+            idCard,
+            city,
+            district,
+            address,
+            firstName,
+            lastName,
+            bankName
+          } = response.data;
+          const user = {
+            id: id,
+            phoneNum: phoneNum,
+            idCard: idCard,
+            city: city,
+            district: district,
+            address: address,
+            bankName: bankName,
+            fullName: `${firstName} ${lastName}`,
+            email: email,
+            image: image
+          };
+          setSession(accessToken);
+
+          dispatch({
+            type: Types.Initial,
+            payload: {
+              isAuthenticated: true,
+              user
+            }
+          });
+        } else {
+          dispatch({
+            type: Types.Initial,
+            payload: {
+              isAuthenticated: false,
+              user: null
+            }
+          });
+        }
+      } catch (err) {
+        console.error(err);
+        dispatch({
+          type: Types.Initial,
+          payload: {
+            isAuthenticated: false,
+            user: null
+          }
+        });
+      }
+    };
+
+    initialize();
+  }, []);
+
+  const login = async () => {
+    const firebaseLogin = await FirebaseService.loginWithGoogle();
+    const firebaseUser = await firebaseLogin.user?.getIdTokenResult();
+    if (!firebaseUser) return;
+    const firebaseToken = firebaseUser.token;
+    console.log(firebaseToken);
+    const response = await axios.post(
+      REACT_APP_API_URL + `authenticate/admin?token=${firebaseToken}`
+    );
+    const {
+      id,
+      token,
+      email,
+      image,
+      phoneNum,
+      idCard,
+      city,
+      district,
+      address,
+      fullName,
+      bankName
+    } = response.data;
+    const user = {
+      id: id,
+      phoneNum: phoneNum,
+      idCard: idCard,
+      city: city,
+      district: district,
+      address: address,
+      bankName: bankName,
+      fullName: fullName,
+      email: email,
+      image: image
+    };
+    setSession(token);
+    window.localStorage.setItem('userId', id);
+    dispatch({
+      type: Types.Login,
+      payload: {
+        user
+      }
+    });
+  };
+
+  const logout = async () => {
+    setSession(null);
+    window.localStorage.removeItem('firebaseToken');
+    dispatch({ type: Types.Logout });
+  };
+
+  const updateProfile = () => {};
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...state,
+        method: 'jwt',
+        login,
+        logout,
+        updateProfile
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthContext, AuthProvider };
