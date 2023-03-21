c3488d7544bc24561defc745594a909fe52ac5d7
TienNguyen-08 <cnnguyenvantien@gmail.com>
2023-03-21 16:15:37 +0700

    update index provider

--- src/index.jsx
+++ src/index.jsx
@@ -6,14 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import { AuthContext, AuthProvider } from './contexts/JWTContext';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
+import {store} from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <AuthProvider> */}
+      <ReduxProvider store={store}>
         <App />
-      {/* </AuthProvider> */}
+      </ReduxProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
