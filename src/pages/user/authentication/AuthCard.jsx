562e92af7391da9f976df9ccf34615649f5f4f5e
huyan3103 <lehuyan04@gmail.com>
2023-04-09 23:41:44 +0700

    Fix bugs

--- src/pages/user/authentication/AuthCard.jsx
+++ src/pages/user/authentication/AuthCard.jsx
@@ -1,36 +1,36 @@
-// import React from "react";
-// import PropTypes from "prop-types";
+import React from "react";
+import PropTypes from "prop-types";
 
-// // material-ui
-// import { Box } from "@mui/material";
+// material-ui
+import { Box } from "@mui/material";
 
-// // project import
-// import MainCard from "../../../components/MainCard";
+// project import
+import { MainCard } from "components/Layout";
 
-// // ==============================|| AUTHENTICATION - CARD WRAPPER ||============================== //
+// ==============================|| AUTHENTICATION - CARD WRAPPER ||============================== //
 
-// const AuthCard = ({ children, ...other }) => (
-//   <MainCard
-//     sx={{
-//       maxWidth: { xs: 400, lg: 475 },
-//       margin: { xs: 2.5, md: 3 },
-//       "& > *": {
-//         flexGrow: 1,
-//         flexBasis: "50%",
-//       },
-//     }}
-//     content={false}
-//     {...other}
-//     border={false}
-//     boxShadow
-//     shadow={(theme) => theme.customShadows.z1}
-//   >
-//     <Box sx={{ p: { xs: 2, sm: 3, md: 4, xl: 5 } }}>{children}</Box>
-//   </MainCard>
-// );
+const AuthCard = ({ children, ...other }) => (
+  <MainCard
+    sx={{
+      maxWidth: { xs: 400, lg: 475 },
+      margin: { xs: 2.5, md: 3 },
+      "& > *": {
+        flexGrow: 1,
+        flexBasis: "50%",
+      },
+    }}
+    content={false}
+    {...other}
+    border={false}
+    boxShadow
+    shadow={(theme) => theme.customShadows.z1}
+  >
+    <Box sx={{ p: { xs: 2, sm: 3, md: 4, xl: 5 } }}>{children}</Box>
+  </MainCard>
+);
 
-// AuthCard.propTypes = {
-//   children: PropTypes.node,
-// };
+AuthCard.propTypes = {
+  children: PropTypes.node,
+};
 
-// export default AuthCard;
+export default AuthCard;
