d3ec807d1cf04dcf830435685e75a3261e5ca49d
Khai Do <64216725+KrisKai@users.noreply.github.com>
2023-04-11 01:04:11 +0700

    add some new apis

---
+++ src/api/trip/plan/tripPlanApi.js
@@ -0,0 +1,26 @@
+import axiosInstance from "../../utils/axios";
+
+const tripPlanApi = {
+  getAll(params) {
+    const url = "/tripPlans";
+    return axiosInstance.get(url, { params });
+  },
+  getById(id) {
+    const url = `/tripPlans/${id}`;
+    return axiosInstance.get(url);
+  },
+  create(data) {
+    const url = "/tripPlans";
+    return axiosInstance.post(url, data);
+  },
+  update(data) {
+    const url = "/tripPlans";
+    return axiosInstance.put(url, data);
+  },
+  delete(id) {
+    const url = `/tripPlans/${id}`;
+    return axiosInstance.delete(url);
+  },
+};
+
+export default tripPlanApi;
