9fec1ff1167921f6b9d9b5c0abaee49693d7c1d6
huyan3103 <lehuyan04@gmail.com>
2023-04-24 19:46:30 +0700

    Update code

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -3,15 +3,17 @@ import Img from "assets/images/footer_img.png";
 
 const Footer = () => {
   return (
-    <Box
-      sx={{
-        backgroundImage: `url(${Img})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        height: "5vh",
-        padding: 10,
-      }}
-    ></Box>
+    <>
+      <Box
+        sx={{
+          backgroundImage: `url(${Img})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          height: "5vh",
+          padding: 10,
+        }}
+      ></Box>
+    </>
   );
 };
 
