d02523f8a5b40f0550157cb44154cc7703198ec8
huyan3103 <lehuyan04@gmail.com>
2023-05-25 22:02:12 +0700

    Update main home page

--- src/components/Home/MainHome/Explore.jsx
+++ src/components/Home/MainHome/Explore.jsx
@@ -1,33 +1,109 @@
-import { Box, TextField, Typography } from "@mui/material";
-import { DatePicker } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers";
+import { Box, Card, CardMedia, IconButton } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Image1 from "assets/images/unsplash_1.png";
+import Image2 from "assets/images/unsplash_2.png";
+import Image3 from "assets/images/unsplash_3.png";
+import Image4 from "assets/images/unsplash_4.png";
+
+const list = [Image1, Image2, Image3, Image4];
+
+const RateCard = (props) => {
+  return (
+    <Card sx={{ minWidth: "295px", minHeight: "200px" }}>
+      <CardMedia image={props.data} sx={{ height: "100%" }}></CardMedia>
+    </Card>
+  );
+};
 
 const Explore = () => {
+  const [test, setTest] = useState(6);
+  const [a, setA] = useState(0);
+  const x = useRef();
+  const y = useRef();
+
+  useEffect(() => {
+    const r = x.current;
+    // console.log(y.current.clientWidth / 3);
+    // console.log((r.clientWidth - 15) / 4);
+    const childCount = r.childElementCount;
+    const scrollWidth = r.childNodes[0].scrollWidth;
+    x.current.scrollTo({
+      top: 0,
+      left: (scrollWidth + 5) * a,
+      behavior: "smooth",
+    });
+  }, [a]);
+
   return (
-    <>
-      <Box>
-        <Typography variant="h2" textTransform="uppercase">
-          tạo hành trình mới
-        </Typography>
-        <Box>
-          <form>
-            <Box>
-              <TextField></TextField>
-            </Box>
-            <Box>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker />
-              </LocalizationProvider>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker />
-              </LocalizationProvider>
-            </Box>
-            <Box></Box>
-          </form>
+    <div>
+      <h1>Khám phá điểm đến ngẫu hứng của bạn</h1>
+      <Box display="flex" alignItems="center" position="relative" py="5px">
+        <Box position="absolute" left="-20px">
+          <IconButton
+            onClick={() => setA(a - 1)}
+            sx={{
+              borderRadius: "50%",
+              paddingLeft: "17px",
+              border: "1px solid black",
+              "&:hover": {
+                backgroundColor: "white",
+              },
+            }}
+          >
+            <ArrowBackIosIcon color="black" />
+          </IconButton>
+        </Box>
+
+        <Box
+          display="flex"
+          gap="10px"
+          py="5px"
+          sx={{
+            overflowX: "scroll",
+            "::-webkit-scrollbar": {
+              display: "none",
+            },
+            scrollbarWidth: "none",
+          }}
+          ref={x}
+        >
+          {list.map((data, idx) => {
+            return <RateCard key={idx} data={data} />;
+          })}
+        </Box>
+
+        <Box position="absolute" right="-17px">
+          <IconButton
+            onClick={() => setA(a + 1)}
+            sx={{
+              borderRadius: "50%",
+              border: "1px solid black",
+              "&:hover": {
+                backgroundColor: "white",
+              },
+            }}
+          >
+            <ArrowForwardIosIcon />
+          </IconButton>
         </Box>
       </Box>
-    </>
+      <Box display="flex" justifyContent="center">
+        <Box backgroundColor="#D1D1D1" ref={y} width="70%" borderRadius="8px">
+          <Box
+            backgroundColor="#000000B2"
+            height="10px"
+            width="283px"
+            borderRadius="8px"
+            sx={{
+              transform: `translateX(${a * 283}px)`,
+              transitionDuration: "0.6s",
+            }}
+          ></Box>
+        </Box>
+      </Box>
+    </div>
   );
 };
 
--- src/components/Home/MainHome/Schedule.jsx
+++ src/components/Home/MainHome/Schedule.jsx
@@ -1,40 +1,31 @@
-import { Box, Card, Grid } from "@mui/material";
+import { Box, Card, CardMedia, Grid } from "@mui/material";
+import Icon1 from "assets/images/icon_1.svg";
+import Icon2 from "assets/images/icon_2.svg";
+import Icon3 from "assets/images/icon_3.svg";
+import Icon4 from "assets/images/icon_4.svg";
+
+const list = [Icon1, Icon2, Icon3, Icon4];
 
 const Schedule = () => {
   return (
     <Grid container spacing={2}>
-      <Grid item md={6}>
-        <Box sx={{ display: "flex" }}>
-          <Card
-            sx={{ border: "1px solid black", width: 300, height: 300 }}
-          ></Card>
-          <div>Lên lịch đi bạn ơi</div>
-        </Box>
-      </Grid>
-      <Grid item md={6}>
-        <Box sx={{ display: "flex" }}>
-          <Card
-            sx={{ border: "1px solid black", width: 300, height: 300 }}
-          ></Card>
-          <div>Lên lịch đi bạn ơi</div>
-        </Box>
-      </Grid>
-      <Grid item md={6}>
-        <Box sx={{ display: "flex" }}>
-          <Card
-            sx={{ border: "1px solid black", width: 300, height: 300 }}
-          ></Card>
-          <div>Lên lịch đi bạn ơi</div>
-        </Box>
-      </Grid>
-      <Grid item md={6}>
-        <Box sx={{ display: "flex" }}>
-          <Card
-            sx={{ border: "1px solid black", width: 300, height: 300 }}
-          ></Card>
-          <div>Lên lịch đi bạn ơi</div>
-        </Box>
-      </Grid>
+      {list.map((data, idx) => {
+        return (
+          <Grid
+            item
+            md={6}
+            sx={{ display: "flex", justifyContent: "center" }}
+            key={idx}
+          >
+            <Box sx={{ display: "flex" }}>
+              <Card sx={{ border: "1px solid black", width: 150, height: 150 }}>
+                <CardMedia image={data} sx={{ height: "100%" }}></CardMedia>
+              </Card>
+              <div>Lên lịch đi bạn ơi</div>
+            </Box>
+          </Grid>
+        );
+      })}
     </Grid>
   );
 };
