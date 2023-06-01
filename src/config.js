10126d41f61571e22ceeb885c36ad505c09426d2
Khai Do <64216725+KrisKai@users.noreply.github.com>
2023-06-01 22:06:58 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const REACT_APP_API_URL =
   'https://localhost:7184/api/v1.0';
-
-export const GOOGLE_MAP_API = "AIzaSyAER9XAP3vNb8HJsYDpUg3gsfq9shZTXlk";
-//AIzaSyBDFdIUGnN989YbBWaeE4qLCzQ5H3mUuEQ
-//key du phong
+export const GOOGLE_MAP_API = "";
+// export const GOOGLE_MAP_API = "AIzaSyBDFdIUGnN989YbBWaeE4qLCzQ5H3mUuEQ";
+//AIzaSyAER9XAP3vNb8HJsYDpUg3gsfq9shZTXlk
+//key cu
 export const PLACE_API = "a4f9fffa383040d581230c5d9fd096b2";
 
 // ==============================|| THEME CONFIG  ||============================== //
