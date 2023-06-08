8041d27ed915f4388d4e65490a23ffd44b5b68bc
Khai Do <64216725+KrisKai@users.noreply.github.com>
2023-06-08 22:47:19 +0700

    update autocomplete

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const REACT_APP_API_URL =
   'https://localhost:7184/api/v1.0';
-export const GOOGLE_MAP_API = "";
-// export const GOOGLE_MAP_API = "AIzaSyBDFdIUGnN989YbBWaeE4qLCzQ5H3mUuEQ";
+// export const GOOGLE_MAP_API = "";
+export const GOOGLE_MAP_API = "AIzaSyBDFdIUGnN989YbBWaeE4qLCzQ5H3mUuEQ";
 //AIzaSyAER9XAP3vNb8HJsYDpUg3gsfq9shZTXlk
 //key cu
 export const PLACE_API = "a4f9fffa383040d581230c5d9fd096b2";
--- src/pages/map/admin/MapForTrip.jsx
+++ src/pages/map/admin/MapForTrip.jsx
@@ -14,6 +14,12 @@ import { useRef, useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import olms from "ol-mapbox-style";
 import * as proj from "ol/proj";
+import { GeocoderAutocomplete } from "@geoapify/geocoder-autocomplete";
+import {
+  GeoapifyGeocoderAutocomplete,
+  GeoapifyContext,
+} from "@geoapify/react-geocoder-autocomplete";
+import '@geoapify/geocoder-autocomplete/styles/minimal.css';
 
 const center = { lat: 16.0545, lng: 108.22074 };
 
@@ -36,8 +42,18 @@ const offices = [
 ];
 
 export default function MapForTrip({ getReturnData, passToProps }) {
-  console.log(1)
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef();
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destinationRef = useRef();
+
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+  const [departure, setDeparture] = useState();
+  const [destination, setDestination] = useState();
+
   let mapContainer;
+
   useEffect(() => {
     const initialState = {
       lng: 108.22074,
@@ -62,11 +78,129 @@ export default function MapForTrip({ getReturnData, passToProps }) {
     });
   }, [mapContainer]);
 
+  function onPlaceSelect(value) {
+    console.log(value);
+  }
+
+  function onSuggectionChange(value) {
+    console.log(value);
+  }
+
+  function preprocessHook(value) {
+    return `${value}, Munich, Germany`
+  }
+
+  function postprocessHook(feature) {
+    return feature.properties.street;
+  }
+
+  function suggestionsFilter(suggestions) {
+    const processedStreets = [];
+
+    const filtered = suggestions.filter(value => {
+      if (!value.properties.street || processedStreets.indexOf(value.properties.street) >= 0) {
+        return false;
+      } else {
+        processedStreets.push(value.properties.street);
+        return true;
+      }
+    })
+
+    return filtered;
+  }
+
+  async function calculateRoute() {
+    if (originRef.current.value === "" || destinationRef.current.value === "") {
+      return;
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    // setDirectionsResponse(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration.text);
+
+    // đây là chỗ đưa dữ liệu ra ngoài component cha
+    const returnData = {
+      startLocationName: originRef.current.value,
+      startLatitude: results.routes[0].legs[0].start_location.lat(),
+      startLongitude: results.routes[0].legs[0].start_location.lng(),
+      endLocationName: destinationRef.current.value,
+      endLatitude: results.routes[0].legs[0].end_location.lat(),
+      endLongitude: results.routes[0].legs[0].end_location.lng(),
+      distance: results.routes[0].legs[0].distance.text,
+      duration: results.routes[0].legs[0].duration.text,
+    };
+    getReturnData(returnData);
+  }
+
   return (
-    <Card
-      className="map-container"
-      ref={(el) => (mapContainer = el)}
-      sx={{ height: "100%", width: "100%" }}
-    />
+    <Box height="90vh" width="100%" display="flex">
+      <Box height="100%" flex="1 1 0" position="relative">
+        <Box
+          bgcolor={"white"}
+          display="flex"
+          justifyContent="space-between"
+          width="94%"
+          position="absolute"
+          zIndex={100}
+          padding={3}
+          margin={4}
+          boxShadow={2}
+          borderRadius={2}
+        >
+          <GeoapifyContext apiKey="a4f9fffa383040d581230c5d9fd096b2">
+            <Grid container>
+              <Grid item xs={12} sm={5}>
+                <GeoapifyGeocoderAutocomplete
+                  placeholder="Trip Start Location"
+                  className="custom-input"
+                  type="street"
+                  lang="vi"
+                  countryCodes="vn"
+                  placeSelect={onPlaceSelect}
+                  postprocessHook={postprocessHook}
+                />
+              </Grid>
+              <Grid item xs={12} sm={5}>
+                <GeoapifyGeocoderAutocomplete
+                  placeholder="Destination Start Location"
+                  className="custom-input"
+                  type="street"
+                  lang="vi"
+                  countryCodes="vn"
+                />
+              </Grid>
+              <Grid item xs={12} sm={2}>
+                <Button
+                  colorScheme="pink"
+                  type="button"
+                  onClick={calculateRoute}
+                  variant="outlined"
+                >
+                  Calculate Route
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={5} mt={2}>
+                <Typography>Distance: {distance} </Typography>
+              </Grid>
+              <Grid item xs={12} sm={5} mt={2}>
+                <Typography>Duration: {duration} </Typography>
+              </Grid>
+            </Grid>
+          </GeoapifyContext>
+        </Box>
+        <Card
+          className="map-container"
+          ref={(el) => (mapContainer = el)}
+          sx={{ height: "100%", width: "100%" }}
+        />
+      </Box>
+    </Box>
   );
 }
